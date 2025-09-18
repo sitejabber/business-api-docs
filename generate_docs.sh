bfb2092e335ddf0ed16283e333c1d0f725e92be3
Alex01d <root01d@gmail.com>
2025-09-19 01:09:38 +0500

    [fix] SJ logo

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 docker pull slatedocs/slate
-docker run --rm --name slate -v $(pwd)/build:/srv/slate/build -v $(pwd)/source/index.html.md:/srv/slate/source/index.html.md -v $(pwd)/source/includes:/srv/slate/source/includes  slatedocs/slate build
+
+docker run --rm --name slate \
+-v $(pwd)/build:/srv/slate/build \
+-v $(pwd)/source/index.html.md:/srv/slate/source/index.html.md \
+-v $(pwd)/source/includes:/srv/slate/source/includes \
+-v $(pwd)/source/images/logo.png:/srv/slate/source/images/logo.png \
+slatedocs/slate build
